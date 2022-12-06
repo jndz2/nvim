224f232add3bf0608b19433ccc08813d95439522
jndz2 <jndzero2@gmail.com>
2022-12-06 21:22:03 +0100

    Fix typo in treesitter module

--- lua/jndz2/plugins/nvim-treesitter.lua
+++ lua/jndz2/plugins/nvim-treesitter.lua
@@ -1,15 +1,14 @@
-local treesitter_config_status, treesitter_config = pcall(require, "nvim-treesitter.config")
-if not treesitter_config_status then
+local treesitter_configs_status, treesitter_configs = pcall(require, "nvim-treesitter.configs")
+if not treesitter_configs_status then
   return
 end
 
-treesitter_config.setup({
+treesitter_configs.setup({
   ensure_installed = {
     "lua",
     "rust",
     "toml",
     "go",
-    "toml",
     "markdown",
     "dockerfile",
     "gitignore",
@@ -19,10 +18,16 @@ treesitter_config.setup({
     "css",
     "yaml",
   },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
   auto_install = true,
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
   },
   ident = { enable = true },
   autopairs = {

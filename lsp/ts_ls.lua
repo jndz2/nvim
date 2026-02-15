437a09669871881c34ef7e72e2c110bbd61eaada
Jan Niklas Dittmar <jndzero2@gmail.com>
2026-02-15 16:21:22 +0100

    feat: add base typescript lsp support

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+   init_options = { hostInfo = 'neovim' },
+   cmd = { 'typescript-language-server', '--stdio' },
+   filetypes = {
+       'javascript',
+       'javascriptreact',
+       'typescript',
+       'typescriptreact',
+    },
+}
